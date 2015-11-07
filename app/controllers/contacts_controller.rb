dd6f70f0c07f282646019bed604e1d51a85013a0
Jackie Luo <jacquelineluo95@gmail.com>
2015-11-07 15:29:52 -0500

    Add Bootstrap

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -71,7 +71,8 @@ class ContactsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
       params.require(:contact).permit(:first_name, :last_name, :company,
-                                      :email, :phone_number, :point_people,
-                                      :involvement, :open_asks, :alumni)
+                                      :email, :phone_number, :point_person,
+                                      :involvement, :open_asks, :alumni,
+                                      :photo_url)
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,12 +6,13 @@ class Contact
   field :company, type: String
   field :email, type: String
   field :phone_number, type: String
-  field :point_people, type: String
+  field :point_person, type: String
   field :involvement, type: String
   field :open_asks, type: String
   field :alumni, type: Boolean
+  field :photo_url, type: String
 
   validates_presence_of :first_name, :last_name, :company, :email,
-                        :point_people, :involvement
+                        :point_person, :involvement
   validates :email, :uniqueness => true
 end
