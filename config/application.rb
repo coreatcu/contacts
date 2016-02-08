830c025d4b608e0d833fd413a719049ade238b4d
Jackie Luo <jacquelineluo95@gmail.com>
2016-02-07 17:45:20 -0800

    Remove iconv

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 require "csv"
-require "iconv"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
